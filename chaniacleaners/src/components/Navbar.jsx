7348b8923efa12685fe3433a2fcbb3651c830ffa
DannyKinaro <kinarodanny15@gmail.com>
2023-01-25 09:36:46 +0300

    initial commit

---
+++ chaniacleaners/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='Logo'>
+        <img src="/images/hoover-img.jpg" alt='hoover cleaning a carpet' />
+    </div>
+  )
+}
+
+export default Navbar
